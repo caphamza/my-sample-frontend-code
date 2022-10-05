d619baa0f7006d68ccb64d3b2ba94491e1f06883
Hamza Aman <hamza.aman@drivers.coop>
2022-10-05 23:25:43 +0500

    sidebar back option added

--- src/layout/sidebar.tsx
+++ src/layout/sidebar.tsx
@@ -1,14 +1,15 @@
-import { useMemo, ReactNode } from "react";
-import { useLocation } from "react-router-dom";
+import { ReactNode } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
-import BackArrow from "assets/icons/back-arrow.png";
+import { ReactComponent as BackArrow } from "assets/icons/back-arrow.svg";
 import { menuItems } from "utils/constants/sidebar";
 import { updateCategory } from "slices/workshopSlice";
 
 import { AppDispatch, RootState } from "store";
 
 const Sidebar = ({ children }: { children: ReactNode }) => {
+  const navigate = useNavigate();
   const { pathname } = useLocation();
   const dispatch: AppDispatch = useDispatch();
   const { category } = useSelector((state: RootState) => state.workshops);
@@ -20,23 +21,40 @@ const Sidebar = ({ children }: { children: ReactNode }) => {
   return (
     <div className="sidebar">
       <div className="sidebar-view">
-        <p>Filter by category:</p>
-        {menuItems.map(({ name, Icon }) => {
-          const isActive = name === category;
-          return (
-            <div
-              key={name}
-              className="sidebar-view-menu"
-              role="button"
-              tabIndex={0}
-              onClick={() => filterWorkshops(name)}
-              onKeyDown={() => filterWorkshops(name)}
-            >
-              {Icon && <Icon fill={isActive ? "#0097CC" : "#1D1D1B"} />}
-              <h5 className={isActive ? "sidebar-view-active" : ""}>{name}</h5>
-            </div>
-          );
-        })}
+        {pathname === "/" ? (
+          <>
+            <p>Filter by category:</p>
+            {menuItems.map(({ name, Icon }) => {
+              const isActive = name === category;
+              return (
+                <div
+                  key={name}
+                  className="sidebar-view-menu"
+                  role="button"
+                  tabIndex={0}
+                  onClick={() => filterWorkshops(name)}
+                  onKeyDown={() => filterWorkshops(name)}
+                >
+                  {Icon && <Icon fill={isActive ? "#0097CC" : "#1D1D1B"} />}
+                  <h5 className={isActive ? "sidebar-view-active" : ""}>
+                    {name}
+                  </h5>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          <div
+            className="sidebar-view-back-view"
+            onClick={() => navigate("/")}
+            onKeyDown={() => navigate("/")}
+            role="button"
+            tabIndex={0}
+          >
+            <BackArrow />
+            <h6>Back</h6>
+          </div>
+        )}
       </div>
       <div>{children}</div>
     </div>
